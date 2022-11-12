4747b3271adbcb0c2ca59f13c4e664f02899d900
FelipeQuinones <fquiones@uc.cl>
2022-11-12 10:41:41 -0300

    transfered some code from video_visual to main

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import cv2
-import matplotlib as plt
-import pandas as pd
+import video_visual as vv
+import graph_visual as gp
+import recorded_df as rdf
 
+cap_ = vv.createCamera()
+
+while(cap_.isOpened()):
+    ret, frame = cap_.read()
+
+    if ret == True:
+       cv2.line(frame, (320, 0), (320, 360), (0, 255, 255), thickness=5, lineType=cv2.LINE_AA)
+       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+       cv2.imshow("frame", frame)
+
+       if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    else:
+        break
+
+cap_.release()
+cv2.destroyAllWindows()
--- video_visual.py
+++ video_visual.py
@@ -6,22 +6,3 @@ def createCamera(x=640, y=360):
     cap.set(3, x)
     cap.set(4, y)
     return cap
-
-cap_ = createCamera()
-
-while(cap_.isOpened()):
-    ret, frame = cap_.read()
-    frame_copy = frame.copy()
-    if ret == True:
-
-       cv2.line(frame_copy, (320, 0), (320, 360), (0, 255, 255), thickness=5, lineType=cv2.LINE_AA)
-       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-       cv2.imshow("frame", frame_copy)
-
-       if cv2.waitKey(1) & 0xFF == ord('q'):
-         break
-    else:
-        break
-
-cap_.release()
-cv2.destroyAllWindows()
