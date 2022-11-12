987e1c0a3dc343642db3f7d42f316e6332fc173c
FelipeQuinones <fquiones@uc.cl>
2022-11-12 04:05:17 -0300

    graph_viual.py

--- graph_visual.py
+++ graph_visual.py
@@ -1,1 +1,39 @@
 import matplotlib as plt
+import time
+import numpy as np
+
+def plot_record(x=0, y=0):
+    plt.ion()
+    # here we are creating sub plots
+    figure, ax = plt.subplots(figsize=(10, 8))
+    line1, = ax.plot(x, y)
+ 
+# setting title
+    plt.title("Geeks For Geeks", fontsize=20)
+ 
+# setting x-axis label and y-axis label
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+ 
+# Loop
+    for _ in range(50):
+    # creating new Y values
+        new_y = np.sin(x-0.5*_)
+ 
+    # updating data values
+        line1.set_xdata(x)
+        line1.set_ydata(new_y)
+ 
+    # drawing updated values
+        figure.canvas.draw()
+ 
+    # This will run the GUI event
+    # loop until all UI events
+    # currently waiting have been processed
+        figure.canvas.flush_events()
+ 
+        time.sleep(0.1)
+
+X = np.linspace(0, 10, 100)
+Y = np.sin(X)
+plot_record(x=X, y=Y)
--- recorded_df.py
+++ recorded_df.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 import time
 
+def pathTime():
+    localtime = time.localtime()
+    df_name = "{hour};{minute};{second}-{day}D-{month}M-{year}Y".format(hour=localtime[3], 
+    minute=localtime[4], second=localtime[5], day=localtime[2], month=localtime[1], year=localtime[0])
+    return str("Records/"+str(df_name)+".csv")
 
-x = int(input())
+def createDf(wght1="Pesa 1", wght2="Pesa 2"):
+    weight1_name, weight2_name = wght1, wght2
+    columns=[weight1_name, weight2_name]
+    df = pd.DataFrame(columns=columns)
+    return df
 
-localtime = time.localtime()
-df_name = "{hour};{minute};{second}-{day}D-{month}M-{year}Y".format(hour=localtime[3], 
-minute=localtime[4], second=localtime[5], day=localtime[2], month=localtime[1], year=localtime[0])
-path_ = str("Records/"+str(df_name)+".csv")
-
-weight1_name, weight2_name = "Peso 1", "Peso 2"
-columns=[weight1_name, weight2_name]
-df = pd.DataFrame(columns=columns)
+df1 = createDf()
 
 for i in range(x):
-    row = {weight1_name:i, weight2_name:i}
-    df = df.append(row, ignore_index=True)
+    row = {"Peso 1":i, "Peso 2":i}
+    df1 = df1.append(row, ignore_index=True)
 
-df.to_csv(path_)
+df1.to_csv(pathTime())
