e0f5d5be47d2b4384e9bd451ee6b22f3e567aaf0
FelipeQuinones <fquiones@uc.cl>
2022-11-14 11:06:16 -0300

    added filter to main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import recorded_df as rdf
 cap_ = vv.createCamera()
 df_ = rdf.createDf()
 
+L_B = np.array([165, 156, 163])
+U_B = np.array([255, 255, 255])
+
 """plt.ion()
 figure, ax = plt.subplots(figsize=(5, 5))
 line1, = ax.plot(np.linspace(0,10,50), range(0,50))
@@ -19,35 +22,36 @@ i = 0
 y_coord1 = [0]
 y_coord2 = [0]
 while(cap_.isOpened()):
-    ret, frame1 = cap_.read()
-    ret, frame2 = cap_.read()
+    ret, frame = cap_.read()
 
     if ret == True:
-        diff = cv2.absdiff(frame1, frame2)
-        gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        inverse = cv2.bitwise_not(gray)
-        _, th1 = cv2.threshold(inverse, 200, 230, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, L_B, U_B)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
         for contour in contours:
-            if cv2.contourArea(contour) < 1000 and cv2.contourArea(contour) > 800:
+            if cv2.contourArea(contour) < 1000000 and cv2.contourArea(contour) > 500:
                 approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
-                cv2.drawContours(frame1, [approx], 0, (0, 0, 0), 5)
+                cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
                 x = approx.ravel()[0]
                 y = approx.ravel()[1] - 5
+                df_ = rdf.addRow(df_, contour[0][0][0], contour[0][0][1])
 
                 if len(approx) == 4:
                     x1 ,y1, w, h = cv2.boundingRect(approx)
                     aspectRatio = float(w)/h
+                    print(contour)
                     if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-                        cv2.putText(frame1, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-
+                        cv2.putText(frame, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
                 else:
-                    cv2.putText(frame1, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+                    cv2.putText(frame, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+                    print(contour)
 
-        cv2.imshow("th1", th1)
-        cv2.imshow("frame", frame1)
-        cv2.imshow("gray", gray)
+        cv2.imshow("frame", frame)
+        cv2.imshow("mask", mask)
+        cv2.imshow("res", res)
 
         """y_coord1.append(contour[0][0][0])
         new_y = y_coord1
@@ -64,7 +68,7 @@ while(cap_.isOpened()):
     else:
         break
 
-    df_ = rdf.addRow(df_, contour[0][0][0], contour[0][0][1])
+    
 
 cap_.release()
 cv2.destroyAllWindows()
